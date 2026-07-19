c9b4ef18b2db4dff64d413233d13b2fcb2b5685f
Vansh <desainutan179@gmail.com>
2026-07-19 12:15:35 -0400

    Add tun device setup and icmp protocol parsing

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <net/if.h>
+#include "../include/tuntap.h"
+#include "../include/ipv4.h" // Added this!
+
+int main() {
+    char tun_name[IFNAMSIZ] = "tun0";
+    int tun_fd = tun_alloc(tun_name);
+    
+    printf("[*] NetStacc Started. Listening on %s...\n", tun_name);
+    unsigned char buffer[2048]; 
+
+    while (1) {
+        int nread = read(tun_fd, buffer, sizeof(buffer));
+        if (nread < 0) continue;
+
+        // Cast the raw bucket into our structured IPv4 glasses
+        struct ipv4_header *ip = (struct ipv4_header *)buffer;
+
+        // Print the protocol number! (1 = ICMP, 6 = TCP, 17 = UDP)
+        printf(" -> Captured %d bytes | Protocol: %d\n", nread, ip->protocol);
+    }
+    return 0;
+}
---
+++ src/tuntap.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/if_tun.h>
+#include <net/if.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/tuntap.h"
+
+int tun_alloc(char *dev) {
+    struct ifreq ifr;
+    int fd, err;
+
+    if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
+        perror("Cannot open /dev/net/tun");
+        exit(1);
+    }
+
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI; 
+
+    if (*dev) {
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+
+    if ((err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0) {
+        perror("ERR: Could not ioctl tun");
+        close(fd);
+        exit(1);
+    }
+
+    return fd;
+}

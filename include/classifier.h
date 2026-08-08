2c18ed6389b55e74e0449490e4f742754210f123
itsmeAishwarya <aishuuv.007@gmail.com>
2026-08-08 11:57:03 +0530

    Add protocol handling functions and update classify_protocol

--- include/classifier.h
+++ include/classifier.h
@@ -4,6 +4,13 @@
 #include <stdint.h>
 #include "ipv4.h"
 
-void classify_protocol(struct ipv4_header *ip, uint8_t *buf, int tun_fd);
+void handle_icmp(struct ipv4_header *ip, uint8_t *buf, int tun_fd);
+
+// UPDATED
+void handle_tcp(struct ipv4_header *ip, uint8_t *payload, int len, int tun_fd, uint8_t *buf);
+
+void handle_udp(struct ipv4_header *ip, uint8_t *payload, int len);
+
+void classify_protocol(struct ipv4_header *ip, uint8_t *buf, int tun_fd, int nread);
 
 #endif
